73d7ae00cf2a48d14fe3aa836891f8e0acf83920
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-12-14 20:22:14 -0500

    intruduced state in React components

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
-  return <div>Hi there!</div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props); // has to be put every time
+
+    // THIS IS THE ONLY TIME wew do direct assignment
+    // to this.state.
+    this.state = { lat: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        // we called setState!!!
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  //  React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
