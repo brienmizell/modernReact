20d9e8839a6c15ec5ba1758b3044616b883ac40a
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-06-10 22:43:46 -0400

    Added life-cycle methods so that video will render even if access directly.

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -7,16 +7,28 @@ class StreamShow extends React.Component {
   constructor(props) {
     super(props);
 
-    this.videoRef = React.createRef;
+    this.videoRef = React.createRef();
   }
 
   componentDidMount() {
+    console.log(this.videoRef);
     const { id } = this.props.match.params;
     this.props.fetchStream(id);
+    this.buildPlayer();
+  }
 
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const { id } = this.props.match.params;
     this.player = flv.createPlayer({
       type: "flv",
-      url: `http://localhost:8000/live/${id}`
+      url: `http://localhost:8000/live/${id}.flv`
     });
     this.player.attachMediaElement(this.videoRef.current);
     this.player.load();
