a5157925502fd6c05361ef151f143dbd2489a396
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-06-12 21:51:44 -0400

    Using the context provider to manage data.

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import UserCreate from "./UserCreate";
+import languageContext from "../contexts/LanguageContext";
 
 class App extends React.Component {
   state = { language: "english" };
@@ -21,7 +22,9 @@ class App extends React.Component {
             onClick={() => this.onLanguageChange("dutch")}
           />
         </div>
-        <UserCreate />
+        <languageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </languageContext.Provider>
       </div>
     );
   }
