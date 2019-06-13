2756f66b91f8a79aa3d153e2412c91c13cf11e28
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-06-13 15:07:28 -0400

    Pulling from multiple contexts.

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import UserCreate from "./UserCreate";
 import languageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class App extends React.Component {
   state = { language: "english" };
@@ -8,6 +9,7 @@ class App extends React.Component {
   onLanguageChange = language => {
     this.setState({ language });
   };
+
   render() {
     return (
       <div className="ui container">
@@ -22,9 +24,11 @@ class App extends React.Component {
             onClick={() => this.onLanguageChange("dutch")}
           />
         </div>
-        <languageContext.Provider value={this.state.language}>
-          <UserCreate />
-        </languageContext.Provider>
+        <ColorContext.Provider value="red">
+          <languageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </languageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
